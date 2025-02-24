972b5bead63f2d43ccd8d57a2136624c8c0b615f
Harry Munyai <harrymunyai@Harrys-MacBook-Air.local>
2025-02-24 06:37:14 +0200

    Initial commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Harambee Student Living",
+  description: "Student accommodation management system",
+  icons: {
+    icon: "/favicon.ico", // Ensures Next.js includes the favicon
+  },
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
